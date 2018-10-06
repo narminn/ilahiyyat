15772c5e34e39ae47fcfe27ed0b80b7f3c9e56f3
narminn <nasirovanarmin96@gmail.com>
2018-10-07 01:05:02 +0400

    asss

---
+++ Ilahiyyat/Areas/Admin_Panel/Controllers/SlidersController.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Ilahiyyat.Models;
+
+namespace Ilahiyyat.Areas.Admin_Panel.Controllers
+{
+    [FilterAdmin]
+    public class SlidersController : Controller
+    {
+        private mecidEntities db = new mecidEntities();
+
+        // GET: Admin/Sliders
+        public ActionResult Index()
+        {
+            return View(db.Slider.ToList());
+        }
+
+        // GET: Admin/Sliders/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Slider slider = db.Slider.Find(id);
+            if (slider == null)
+            {
+                return HttpNotFound();
+            }
+            return View(slider);
+        }
+
+        // GET: Admin/Sliders/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/Sliders/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,slider_şəkili,slider_məzmunu")] Slider slider, HttpPostedFileBase slider_şəkili)
+        {
+            if (ModelState.IsValid)
+            {
+                Random random = new Random();
+                int randomNumber = random.Next(0, 100000);
+                var filename = Path.GetFileName(slider_şəkili.FileName);
+                var src = Path.Combine(Server.MapPath("~/Uploads/"), randomNumber + filename);
+                slider_şəkili.SaveAs(src);
+                slider.slider_şəkili = "/Uploads/" + randomNumber + filename;
+                db.Slider.Add(slider);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(slider);
+        }
+
+        // GET: Admin/Sliders/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Slider slider = db.Slider.Find(id);
+            if (slider == null)
+            {
+                return HttpNotFound();
+            }
+            return View(slider);
+        }
+
+        // POST: Admin/Sliders/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,slider_şəkili,slider_məzmunu")] Slider slider, HttpPostedFileBase slider_şəkili)
+        {
+            if (ModelState.IsValid)
+            {
+                Random random = new Random();
+                int randomNumber = random.Next(0, 100000);
+                var filename = Path.GetFileName(slider_şəkili.FileName);
+                var src = Path.Combine(Server.MapPath("~/Uploads/"), randomNumber + filename);
+                slider_şəkili.SaveAs(src);
+                slider.slider_şəkili = "/Uploads/" + randomNumber + filename;
+                db.Entry(slider).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(slider);
+        }
+
+        // GET: Admin/Sliders/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Slider slider = db.Slider.Find(id);
+            if (slider == null)
+            {
+                return HttpNotFound();
+            }
+            return View(slider);
+        }
+
+        // POST: Admin/Sliders/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Slider slider = db.Slider.Find(id);
+            db.Slider.Remove(slider);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
